bf4e09bf844031261d207bad53dc88a1ddbe00e2
Sai Srinivas Lakkakula <saisrinu.lakkakula@gmail.com>
2025-09-28 23:49:15 -0700

    add GH actions CI yml

--- exercise_2/__main__.py
+++ exercise_2/__main__.py
@@ -1,11 +1,11 @@
-import Calculator
+import exercise_2.src.Calculator as calculator
 def run_module():
-    obj = Calculator.Calculator(5,3)
+    obj = calculator.Calculator(5,3)
     return obj.add()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    run_module()
+    print(run_module())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
